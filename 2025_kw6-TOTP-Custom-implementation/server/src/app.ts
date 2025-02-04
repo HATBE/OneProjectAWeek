05ba43459eb5bfe14347c792f7d6a751acb65637
HATBE2113 <admin@aarongensetter.ch>
2025-02-04 21:11:23 +0100

    otp

--- 2025_kw6-TOTP-Custom-implementation/server/src/app.ts
+++ 2025_kw6-TOTP-Custom-implementation/server/src/app.ts
@@ -4,71 +4,63 @@ const period = 30;
 
 const base32tohex = (base32) => {
   const base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+  const cleanBase32 = base32.replace(/=+$/, "").toUpperCase();
 
-  let bits = "";
+  const bits = [...cleanBase32]
+    .map((char) => {
+      const val = base32chars.indexOf(char);
+      if (val === -1) throw new Error("Invalid base32 character in key");
+      return val.toString(2).padStart(5, "0");
+    })
+    .join("");
 
-  let hex = "";
-
-  const _base32 = base32.replace(/=+$/, "");
-
-  for (let i = 0; i < _base32.length; i++) {
-    const val = base32chars.indexOf(base32.charAt(i).toUpperCase());
-
-    if (val === -1) throw new Error("Invalid base32 character in key");
-
-    bits += val.toString(2).padStart(5, "0");
-  }
-
-  for (let i = 0; i + 8 <= bits.length; i += 8) {
-    const chunk = bits.substr(i, 8);
-
-    hex = hex + parseInt(chunk, 2).toString(16).padStart(2, "0");
-  }
-
-  return hex;
+  return (
+    bits
+      .match(/.{8}/g)
+      ?.map((byte) => parseInt(byte, 2).toString(16).padStart(2, "0"))
+      .join("") || ""
+  );
 };
 
 const generateTOTP = (key: string) => {
-  const timestamp = Date.now();
-  const epoch = Math.floor(timestamp) / 1000;
+  const timestamp = Math.floor(Date.now() / 1000);
 
-  let time = Math.round(epoch / period)
+  let time = Math.round(Math.floor(timestamp / period))
     .toString(16)
+    .padStart(2, "0")
     .padStart(16, "0");
 
   const sha = new jsSHA("SHA-1", "HEX");
 
-  sha.setHMACKey(base32tohex(key), "HEX"); //fix
+  sha.setHMACKey(base32tohex(key), "HEX");
   sha.update(time);
 
   const hmac = sha.getHMAC("HEX");
 
-  const offset = parseInt(hmac.substring(hmac.length - 1), 16);
+  const offset = parseInt(hmac.slice(-1), 16);
 
   let otp = (
-    parseInt(hmac.substring(offset * 2, 8), 16) & parseInt("7fffffff", 16)
+    parseInt(hmac.slice(offset * 2, offset * 2 + 8), 16) &
+    parseInt("7fffffff", 16)
   ).toString();
 
   const start = Math.max(otp.length - 6, 0);
 
   otp = otp.substring(start, start + 6);
 
-  const expires =
-    Math.ceil((timestamp + 1) / (timestamp * 1000)) * timestamp * 1000;
+  const expires = Math.ceil((timestamp + 1) / (period * 1000)) * period * 1000;
 
   return { otp, expires };
 };
 
-console.log(generateTOTP("JBSWY3DPEHPK3PXP"));
-
-/*
 import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {});
+app.get("/:token", (req, res) => {
+  res.send(generateTOTP(req.params.token));
+});
 
 app.listen(port, () => {
   return console.log(`App is listening at http://localhost:${port}`);
 });
-*/
